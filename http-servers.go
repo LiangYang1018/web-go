77dad92db8c012133b8c9c8ed27cd8609db97583
杨亮 <yangl17@chinatelecom.cn>
2022-09-14 20:18:09 +0800

    post data

--- http-servers.go
+++ http-servers.go
@@ -14,13 +14,16 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	case "GET":
 		fmt.Println("it is a get request")
 		fruit := req.FormValue("fruit")
-		fmt.Fprintf(w, "%s receieved!", fruit)
+		fmt.Fprintf(w, "%s Got!", fruit)
 
 	case "POST":
-		req.ParseForm()
-		fmt.Println("it is a post request")
-		s, _ := ioutil.ReadAll(req.Body)
-		fmt.Fprintf(w, "%s", s)
+
+		body, _ := ioutil.ReadAll(req.Body)
+		if len(body) > 0 {
+			fmt.Println("--------------it is a post request--------------")
+			fmt.Println(string(body))
+			w.Write([]byte("posted success!"))
+		}
 
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
