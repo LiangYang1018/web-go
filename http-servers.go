f93c7182af374d99b42d90a75b02dfbf91f31590
杨亮 <yangl17@chinatelecom.cn>
2022-09-18 20:54:46 +0800

    解析json

--- http-servers.go
+++ http-servers.go
@@ -3,11 +3,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"net/http/httputil"
-	"regexp"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -20,30 +18,30 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 	case "POST":
 
-		//body, _ := ioutil.ReadAll(req.Body)
-		//if len(body) > 0 {
-		//	fmt.Println("--------------it is a post request--------------")
-		//	fmt.Println(string(body))
-		//	w.Write([]byte("posted success!"))
-		//}
-		requestDump, err := httputil.DumpRequest(req, true)
-		if err != nil {
-			fmt.Println(err)
+		body, _ := ioutil.ReadAll(req.Body)
+		if len(body) > 0 {
+			fmt.Println("--------------it is a post request--------------")
+			fmt.Println(string(body))
+			w.Write([]byte("posted success!"))
 		}
-		bodyData := string(requestDump)
-		r, _ := regexp.Compile("{[\\s\\S]*}")
-		bodyJson := r.FindString(bodyData)
-		fmt.Println("--------------post body----------------")
-		fmt.Println(bodyJson)
-		fmt.Println("--------------json字段样例--------------")
-		jsonData := []byte(bodyJson)
-		var v interface{}
-		json.Unmarshal(jsonData, &v)
-		data := v.(map[string]interface{})
-		fmt.Println("cluster: ", data["cluster"])
-		fmt.Println("rule_name: ", data["rule_name"])
-		fmt.Println("first_trigger_time: ", data["first_trigger_time"])
-		fmt.Println("is_recovered: ", data["is_recovered"])
+		//requestDump, err := httputil.DumpRequest(req, true)
+		//if err != nil {
+		//	fmt.Println(err)
+		//}
+		//bodyData := string(requestDump)
+		//r, _ := regexp.Compile("{[\\s\\S]*}")
+		//bodyJson := r.FindString(bodyData)
+		//fmt.Println("--------------post body----------------")
+		//fmt.Println(bodyJson)
+		//fmt.Println("--------------json字段样例--------------")
+		//jsonData := []byte(bodyJson)
+		//var v interface{}
+		//json.Unmarshal(jsonData, &v)
+		//data := v.(map[string]interface{})
+		//fmt.Println("cluster: ", data["cluster"])
+		//fmt.Println("rule_name: ", data["rule_name"])
+		//fmt.Println("first_trigger_time: ", data["first_trigger_time"])
+		//fmt.Println("is_recovered: ", data["is_recovered"])
 
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
